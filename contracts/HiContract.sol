a394aa55adb1f587f3fd9a2b6fa0faf5aca53865
Minh Dong <minhdn1210@gmail.com>
2017-08-13 21:18:10 -0700

    add sample contract

--- contracts/HiContract.sol
+++ contracts/HiContract.sol
@@ -1,8 +1,29 @@
 pragma solidity ^0.4.4;
 
 contract HiContract{
-  uint public balance;
+  address public owner;
+  mapping(address => uint) balances;
+
+  // constructor run upon contract creation 
   function HiContract() {
-    balance = 1500;
+    // when contract first deploy, set owner
+    owner = msg.sender; // address that sending the transaction
+    balances[owner] = 1000; // initial value for owner balance of 1000
+  }
+
+  function getBalance(address user) constant returns (uint) { 
+    // constant since this function will not change state of object
+    return balances[user];
+  }
+
+  function transfer(address to, uint value) returns (bool) {
+    address from = msg.sender;
+    // check to see if from has enough money to send
+    if (balances[from] >= value) {
+      balances[from] = balances[from] - value;
+      balances[to] = balances[to] + value;
+      return true;
+    }
+    return false;
   }
 }
