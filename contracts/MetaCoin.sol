da1baf99bcde6ddbe21313f1f3c293dc870ee54d
ninjacatt <minhdevspace@gmail.com>
2017-08-16 22:18:18 -0700

    Update metazoan

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -8,23 +8,49 @@ import "./ConvertLib.sol";
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
 contract MetaCoin {
+	address minter;
 	mapping (address => uint) balances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	function MetaCoin() {
-		balances[tx.origin] = 10000;
+		minter = msg.sender;
+		balances[minter] = 10000;
+	}
+
+	function mint(address receiver, uint amount) returns(bool mintSucceed) {
+		// Only owner of contract could add money to an address
+		if (minter != msg.sender) {
+			return false;
+		}
+
+		balances[receiver] = amount;
+		return true;
 	}
 
 	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
+		if (balances[msg.sender] < amount) {
+			return false;
+		}
 		balances[msg.sender] -= amount;
 		balances[receiver] += amount;
+		// Transfer event will be fired when coin being sent successfully
+		// to listen to this event, you could do
+		// MetaCoin.Transfer().watch({}, '', function(error, result) {
+		//		if (!error) {
+		//				console.log("Coin transfer: " + result.args._amount +
+		//						" coins were sent from " + result.args._from +
+		//						" to " + result.args._to + ".");
+		//				console.log("Balances now:\n" +
+		//						"Sender: " + MetaCoin.getBalance(result.args.from) +
+		//						"Receiver: " + MetaCoin.getBalance(result.args.to));
+		//		}
+		//})
 		Transfer(msg.sender, receiver, amount);
 		return true;
 	}
 
-	function getBalanceInEth(address addr) returns(uint){
+	function getBalanceInEth(address addr) returns(uint) {
 		return ConvertLib.convert(getBalance(addr),2);
 	}
 
